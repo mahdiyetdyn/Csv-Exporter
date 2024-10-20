b737ef4c2aa290c25afee5d9fa2b1ad82166300b
mahdiye <tadayon.mahdiye@gmail.com>
2024-10-20 12:13:25 +0330

    Clean Code

--- src/main/java/com/tadayon/csvexporter/service/CsvExporterService.java
+++ src/main/java/com/tadayon/csvexporter/service/CsvExporterService.java
@@ -16,7 +16,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -27,49 +26,54 @@ public class CsvExporterService {
     private final CsvConfig csvConfig;
 
     @PostConstruct
-    public void init() throws IOException {
-        log.info("in post construct");
-        exportToCsv(csvConfig.getFilePath());
+    public void init(){
+        log.info("Initializing CSV export");
+        try {
+            exportToCsv(csvConfig.getFilePath());
+        } catch (IOException e) {
+            log.error("Failed to export CSV", e);
+        }
     }
 
     public void exportToCsv(String filePath) throws IOException {
+        long startTime = System.currentTimeMillis();
+        log.info("Export started at: {}", startTime);
+
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
             writer.write(CsvConstant.HEADER);
 
             int pageNumber = 0;
-            boolean hasMoreRecords = true;
-            long startTime, endTime;
-
-            log.info("start time: {}", startTime = System.currentTimeMillis());
+            Page<BlackList> page;
             Timestamp timestamp = new Timestamp(0);
-            while (hasMoreRecords) {
-                // Fetch pages
-                Page<BlackList> page = blackListRepository
-                        .findAllByOperationDateGreaterThanEqualOrderByOperationDateAsc(
-                                timestamp.toLocalDateTime(),
-                                PageRequest.of(pageNumber, csvConfig.getBatchSize()));
 
-                List<BlackList> blackLists = page.getContent();
-
-                // Batch Write Records
-                StringBuilder sb = new StringBuilder();
-                for (BlackList blackList : blackLists) {
-                    // Write each record as a CSV line
-                    sb.append(recordToCsvLine(blackList)).append("\n");
-                }
-                writer.write(sb.toString());
+            do {
+                page = fetchPage(timestamp.toLocalDateTime(), pageNumber);
+                String csvContent = convertPageToCsv(page);
+                writer.write(csvContent);
                 writer.flush();
 
-                hasMoreRecords = page.hasNext();
+                log.info("Processed page: {}", pageNumber);
                 pageNumber++;
-            }
-            log.info("end time: {}", endTime = System.currentTimeMillis());
-            log.info("duration: {}", endTime - startTime);
+
+            } while (page.hasNext());
+
+            long endTime = System.currentTimeMillis();
+            log.info("Export completed at: {}. Duration: {} ms", endTime, endTime - startTime);
         }
     }
 
-    private String convertPageToCsv(List<BlackList> blackLists) {
-        return blackLists.stream()
+    private Page<BlackList> fetchPage(LocalDateTime timestamp, int pageNumber) {
+        return blackListRepository
+                .findAllByOperationDateGreaterThanEqualOrderByOperationDateAsc(
+                        timestamp,
+                        PageRequest.of(
+                                pageNumber,
+                                csvConfig.getBatchSize())
+                );
+    }
+
+    private String convertPageToCsv(Page<BlackList> page) {
+        return page.getContent().stream()
                 .map(this::recordToCsvLine)
                 .collect(Collectors.joining());
     }
